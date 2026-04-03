99d4196bcc8e17381195938650ba5011bf96793c
AguaIgnea <amayaerick030@gmail.com>
2026-04-03 10:56:02 -0500

    ajuste a validaciones de companies usando express-validator

--- src/routes/companies.js
+++ src/routes/companies.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { body } from 'express-validator';
 import { getCompanies, getCompanyById, createCompany, updateCompany, deleteCompany } from '../controllers/companies.js';
 import { authenticate, requireRole } from '../middlewares/webToken.js';
 
@@ -11,12 +12,27 @@ router.get('/', getCompanies);
 router.get('/:id', getCompanyById);
 
 // POST /companies
-router.post('/', createCompany);
+//validaciones con express-validator
+router.post( '/',
+  [
+    body('nombre')
+      .notEmpty().withMessage('El nombre es obligatorio'),
+
+    body('email_contacto')
+      .isEmail().withMessage('Debe ser un email válido'),
+
+    body('plan')
+      .optional()
+      .isIn(['free', 'pro', 'enterprise']).withMessage('Plan inválido')
+  ],
+  createCompany
+);
 
 // PUT /companies/:id
 router.put('/:id', updateCompany);
 
 // DELETE /companies/:id
 router.delete('/:id', authenticate, requireRole('admin'), deleteCompany);
+
 
 export default router;
